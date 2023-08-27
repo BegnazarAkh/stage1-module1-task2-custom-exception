4ce88eab17faaf8567d6657159d6df35f70c16fa
BegnazarAkh <begnazaraxmadjonov@mail.ru>
2023-08-27 16:59:47 +0200

    Stage 1, Task 2

--- src/main/java/com/epam/mjc/StudentManager.java
+++ src/main/java/com/epam/mjc/StudentManager.java
@@ -1,21 +1,27 @@
 package com.epam.mjc;
-
-
-public class StudentManager {
+import com.epam.mjc.StudentNotFoundException;
+public class StudentManager  {
 
   private static final long[] IDs = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
 
-  public Student find(long studentID) {
-    return Student.getValueOf(studentID);
+  public Student find(long studentID) throws StudentNotFoundException {
+    for (long id : IDs) {
+      if (id == studentID) {
+        return Student.getValueOf(studentID);
+      }
+    }
+    throw new StudentNotFoundException("Could not find student with ID " + studentID);
   }
 
   public static void main(String[] args) {
     StudentManager manager = new StudentManager();
-
     for (int i = 0; i < IDs.length; i++) {
-      Student student = manager.find(IDs[i]);
-      System.out.println("Student name " + student.getName());
+      try{
+          Student student = manager.find(IDs[i]);
+          System.out.println("Student name " + student.getName());
+      } catch (StudentNotFoundException ex){
+        System.err.print(ex);
+      }
     }
-
   }
 }
